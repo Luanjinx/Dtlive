8c5c6ae58c10e18c1db03e6e69e6dcf042f49240
Luanjinx <sahruldarmian97@gmail.com>
2026-09-22 03:39:17 +0700

    Update webcontentshowdetails.dart

--- lib/webpages/webcontentshowdetails.dart
+++ lib/webpages/webcontentshowdetails.dart
@@ -386,7 +386,9 @@ class WebContentShowDetailsState extends State<WebContentShowDetails>
   Future<void> openPlayer(String playType) async {
     printLog("mCurrentEpiPos ========> ${showDetailsProvider.mCurrentEpiPos}");
     if ((episodeProvider.episodeList?.length ?? 0) > 0) {
-      if (showDetailsProvider.mCurrentEpiPos == -1) return;
+      if (showDetailsProvider.mCurrentEpiPos == -1) {
+        showDetailsProvider.mCurrentEpiPos = 0;
+      }
       /* CHECK SUBSCRIPTION */
       if (playType != "Trailer") {
         bool? isPrimiumUser = await Utils.checkSubsRentLogin(
