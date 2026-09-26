2908771c1cc919d5746bc753941c54b01d364a3d
Luanjinx <sahruldarmian97@gmail.com>
2026-09-26 12:49:42 +0700

    update

--- lib/widget/episodebyseason.dart
+++ lib/widget/episodebyseason.dart
@@ -1015,7 +1015,7 @@ class _EpisodeBySeasonState extends State<EpisodeBySeason> {
                     if (v1080.isNotEmpty) _buildQualityOption(context, "1080p", "Full HD", v1080, position, episode, false),
                     
                     const SizedBox(height: 10),
-                    Divider(color: otherColor, thickness: 1),
+                    Divider(color: Colors.grey.withOpacity(0.5), thickness: 1),
                     const SizedBox(height: 10),
                     
                     if (v320.isNotEmpty || v480.isNotEmpty || v720.isNotEmpty || v1080.isNotEmpty)
